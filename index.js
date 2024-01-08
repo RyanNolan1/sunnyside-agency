8810755d6cf92590f21d5170f991f17976da61f6
Ryan <ryannolan1990@hotmail.com>
2024-01-08 16:58:03 +0000

    this will fix a bug where the page moves up when the menu is opened

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const burgerNav = document.querySelectorAll(".icon-mobile-nav");
 const mobileMenu = document.getElementById('mobile-menu-container');
+const closeButton = document.querySelector('.icon-mobile-nav[name="close"]')
+const openButton = document.querySelector('.icon-mobile-nav[name="menu"]')
 
 burgerNav.forEach(function (button) {
     button.addEventListener("click", mobileToggle);
@@ -8,10 +10,15 @@ burgerNav.forEach(function (button) {
 function mobileToggle() {
      if (parseFloat(mobileMenu.style.opacity) === 0) {
         mobileMenu.style.opacity = 1;
-        mobileMenu.style.translate = "-100%"
+        mobileMenu.style.translate = "-100%";
+        openButton.style.display = "none";
+        closeButton.style.display = "flex"
     } else if (parseFloat(mobileMenu.style.opacity) === 1) {
         mobileMenu.style.opacity = 0;
         mobileMenu.style.translate = "+100%"
+        openButton.style.display = "flex";
+        closeButton.style.display = "none"
+
     }
 }
 
