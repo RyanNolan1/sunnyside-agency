df196307ddebe224dc4e935440b201c1ca2a6865
Ryan <ryannolan1990@hotmail.com>
2024-01-08 16:17:52 +0000

    this makes the menu fade in and out on click

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const burgerNav = document.querySelectorAll(".icon-mobile-nav");
 const mobileMenu = document.getElementById('mobile-menu-container');
 
 burgerNav.forEach(function (button) {
-button.addEventListener("click", mobileToggle)
-})
+    button.addEventListener("click", mobileToggle);
+});
 
 function mobileToggle() {
-if (mobileMenu.style.display === "") {
-        mobileMenu.style.display = "flex";
-} else if (mobileMenu.style.display === "flex") {
-        mobileMenu.style.display = "";
-}
+     if (parseFloat(mobileMenu.style.opacity) === 0) {
+        mobileMenu.style.opacity = 1;
+    } else if (parseFloat(mobileMenu.style.opacity) === 1) {
+        mobileMenu.style.opacity = 0;
+    }
 }
